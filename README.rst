f1695714447047dd1b97f420166fa32a6a2569bb
cdfarrow <flextoolshelp@gmail.com>
2018-12-11 16:41:07 +1300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,13 @@
+cdfutils
+========
+
+cdfutils is a library of miscellaneous utility functions and classes
+that I've created for various projects. 
+
+ConfigStore:
+    A generic configuration class that persists values in a text file.
+    
+DotNetForms:
+    Some general helper classes for use with .NET Windows.Forms:
+        CustomMainMenu
+        CustomToolBar
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE.txt') as f:
+    license = f.read()
+
+setup(
+    name='cdfutils',
+    version='1.0.1',
+    description='General utility library of miscellaneous functions and classes',
+    long_description=readme,
+    author='Craig Farrow',
+    author_email='cdfkiwi@gmail.com',
+    url='https://github.com/cdfarrow/cdfutils',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
