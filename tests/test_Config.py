1abf037d58b720a3383e182feec96279bbf788ff
cdfarrow <flextoolshelp@gmail.com>
2019-07-26 10:42:58 +1200

    Update test_Config.py
    Refresh value setup for each test

--- tests/test_Config.py
+++ tests/test_Config.py
@@ -15,21 +15,20 @@ class TestConfig(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.config = ConfigStore(CONFIG_FILENAME)
+
+    @classmethod
+    def tearDownClass(self):
+        del self.config     # Close the file
+        os.remove(CONFIG_FILENAME)
+
+    def setUp(self):
         self.config.username = CFG_USERNAME
         self.config.email    = CFG_EMAIL
         self.config.users    = CFG_USERS
         self.config.family   = CFG_FAMILY
-        del self.config     # Close the file
-
-    @classmethod
-    def tearDownClass(self):
-        os.remove(CONFIG_FILENAME)
-
-    def setUp(self):
-        self.config = ConfigStore(CONFIG_FILENAME)
 
     def tearDown(self):
-        del self.config
+        pass
 
     def test_GetValues(self):
         self.assertEqual(self.config.username, CFG_USERNAME)
@@ -39,14 +38,11 @@ class TestConfig(unittest.TestCase):
 
     def test_ChangeValues(self):
         NEWNAME = "Barney"
-        oldname = self.config.username
         self.config.username = NEWNAME
         self.assertEqual(self.config.username, NEWNAME)
-        self.config.username = oldname  # Restore
 
         self.config.users += 1
         self.assertEqual(self.config.users   , CFG_USERS+1)
-        self.config.users -= 1
 
 if __name__ == "__main__":
     unittest.main()
