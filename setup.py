5cfa22ab1f2631bf3928713dea44204cc99abf2c
cdfarrow <flextoolshelp@gmail.com>
2019-09-17 17:26:33 +1200

    Bump version for handling of None value for keyboard shortcut

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE.txt') as f:
 
 setup(
     name='cdfutils',
-    version='1.0.1',
+    version='1.0.2',
     description='General utility library of miscellaneous functions and classes',
     long_description=readme,
     author='Craig Farrow',
