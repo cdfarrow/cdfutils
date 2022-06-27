cea2407b04ff095f40515fc974eea91f09411a6f
cdfarrow <cdfarrow@users.noreply.github.com>
2022-06-27 17:14:05 +1200

    Update tests for changed function names

--- tests/test_Textfile.py
+++ tests/test_Textfile.py
@@ -5,19 +5,20 @@ import unittest
 from cdfutils import Textfile
 
 
-TEST_FILENAME = r"testQuotes.txt"
+TEST_FILENAME = os.path.join(os.path.dirname(__file__), 
+                             r"testQuotes.txt")
 
 class TestConfig(unittest.TestCase):
 
-    def test_randomSection(self):
+    def test_RandomSection(self):
         with open(TEST_FILENAME) as f:
             wholeFile = f.read()
-            self.assertTrue(Textfile.randomSection(f) in wholeFile)
+            self.assertTrue(Textfile.RandomSection(f) in wholeFile)
 
-    def test_randomLine(self):
+    def test_RandomLine(self):
         with open(TEST_FILENAME) as f:
             wholeFile = f.read()
-            self.assertTrue(Textfile.randomLine(f) in wholeFile)
+            self.assertTrue(Textfile.RandomLine(f) in wholeFile)
 
 if __name__ == "__main__":
     unittest.main()
