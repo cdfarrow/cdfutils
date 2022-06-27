7a1b24fd91254f3c1d2ee92890891967487319ce
cdfarrow <cdfarrow@users.noreply.github.com>
2022-06-27 18:25:42 +1200

    Revert "Update tests for changed function names"
    This reverts commit cea2407b04ff095f40515fc974eea91f09411a6f.

--- tests/test_Textfile.py
+++ tests/test_Textfile.py
@@ -5,20 +5,19 @@ import unittest
 from cdfutils import Textfile
 
 
-TEST_FILENAME = os.path.join(os.path.dirname(__file__), 
-                             r"testQuotes.txt")
+TEST_FILENAME = r"testQuotes.txt"
 
 class TestConfig(unittest.TestCase):
 
-    def test_RandomSection(self):
+    def test_randomSection(self):
         with open(TEST_FILENAME) as f:
             wholeFile = f.read()
-            self.assertTrue(Textfile.RandomSection(f) in wholeFile)
+            self.assertTrue(Textfile.randomSection(f) in wholeFile)
 
-    def test_RandomLine(self):
+    def test_randomLine(self):
         with open(TEST_FILENAME) as f:
             wholeFile = f.read()
-            self.assertTrue(Textfile.RandomLine(f) in wholeFile)
+            self.assertTrue(Textfile.randomLine(f) in wholeFile)
 
 if __name__ == "__main__":
     unittest.main()
